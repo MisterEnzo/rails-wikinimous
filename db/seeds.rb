c56d96aea981af93f571c2641823e2d2299e0db0
Enrique Naguiat <enzo_naguiat@yahoo.com>
2017-10-10 15:56:54 +0900

    seeded with 10 articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::GameOfThrones.character,
+    content: Faker::Lorem.paragraph(2, true)
+  )
+  article.save
+end
+
+puts "Created 10 new articles"
